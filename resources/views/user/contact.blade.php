b9af3966c317614cb2aa32eaf6e7d150c1703098
spyreddd <adyanizza5353@gmail.com>
2024-05-24 21:05:57 +0700

    company profile

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -25,7 +25,7 @@
                         </div>
                         <div class="contact_text">
                             <span>Email Address</span>
-                            <a href="neoparadigmcomicsindonesia@gmail.com">{{env('APP_EMAIL')}} </a>
+                            <a href="mailto:neoparadigmcomicsindonesia@gmail.com">{{env('APP_EMAIL')}} </a>
                         </div>
                     </div>
                 </div>
@@ -44,6 +44,7 @@
         </div>
     </div>
     <!-- END SECTION CONTACT -->
+
     <!-- START SECTION CONTACT -->
     <div class="section pt-0">
         <div class="container">
@@ -57,5 +58,142 @@
             </div>
         </div>
     </div>
-    <!-- END SECTION CONTACT -->
+
+<!-- START SECTION NPC -->
+<div class="section pb_20">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 ">
+                <div class="p-3 single_banner text-white">
+                    <img src="{{asset('assets/images/npc1.png')}}" alt="npc1" />
+                    <div class="single_banner_info">
+                    </div>
+                </div>
+            </div>
+                <div class="col-md-6">
+                    <div class="single_banner">
+                        <img src="{{asset('assets/images/npc3.png')}}" alt="npc3" />
+                        <div class="single_banner_info">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END SECTION NPC -->
+
+        <!-- START SECTION VISI DAN MISI -->
+        <div class="section pt-0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="heading_s1">
+                            <h2>Visi dan Misi</h2>
+                        </div>
+                        <h3>Visi</h3>
+                        <p>Menjadi studio komik profesional yang mampu bersaing, baik di lingkup lokal maupun internasional.</p>
+                        <h3>Misi</h3>
+                        <ol>
+                            <li>Memuliakan komik Indonesia sebagai salah satu bentuk karya seni dan media berekspresi.</li>
+                            <li>Meningkatkan kecintaan masyarakat terhadap komik Indonesia melalui penerbitan karya-karya komik yang bermutu dan kontinyu.</li>
+                            <li>Membina dan menjadi wadah bagi para talent dalam mempublikasikan karya-karyanya secara profesional.</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        <!-- END SECTION VISI DAN MISI -->
+
+
+        <!-- START SECTION EMPLOYEE -->
+        <div class="section pb_70">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-md-6">
+                        <div class="contact_wrap contact_style3">
+                            <img src="{{asset('assets/images/Dwi Aspitono.png')}}" alt="dwi" />
+                            <div class="contact_text">
+                                <span>Dwi Aspitono</span>
+                                <li class="d-flex justify-content-center" style="list-style-type: none; margin: 0; padding: 0;">
+                                    as
+                                </li>
+                                <li class="d-flex justify-content-center fw-bold" style="list-style-type: none; margin: 0; padding: 0; color: rgb(255, 50, 77);">
+                                    Penciler, Inker, Writer
+                                </li>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="contact_wrap contact_style3">
+                            <img src="{{asset('assets/images/Berny Julianto.png')}}" alt="berny" />
+                            <div class="contact_text">
+                                <span>Berny Julianto</span>
+                                <li class="d-flex justify-content-center" style="list-style-type: none; margin: 0; padding: 0;">
+                                    as
+                                </li>
+                                <li class="d-flex justify-content-center fw-bold" style="list-style-type: none; margin: 0; padding: 0; color: rgb(255, 50, 77);">
+                                    Writer, Colorist, Editor, CEO
+                                </li>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="contact_wrap contact_style3">
+                            <img src="{{asset('assets/images/Kasmiyanto Samad.png')}}" alt="gino" />
+                            <div class="contact_text">
+                                <span>Kasmiyanto Samad (Gino)</span>
+                                <li class="d-flex justify-content-center" style="list-style-type: none; margin: 0; padding: 0;">
+                                    as
+                                </li>
+                                <li class="d-flex justify-content-center fw-bold" style="list-style-type: none; margin: 0; padding: 0; color: rgb(255, 50, 77);">
+                                    Penciler, Inker
+                                </li>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="contact_wrap contact_style3">
+                            <img src="{{asset('assets/images/Arief Hargono.png')}}" alt="arief" />
+                            <div class="contact_text">
+                                <span>Arief Hargono</span>
+                                <li class="d-flex justify-content-center" style="list-style-type: none; margin: 0; padding: 0;">
+                                    as
+                                </li>
+                                <li class="d-flex justify-content-center fw-bold" style="list-style-type: none; margin: 0; padding: 0; color: rgb(255, 50, 77);">
+                                    Penciler, Writer
+                                </li>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="contact_wrap contact_style3 d-none"> <!-- Add d-none class here -->
+                            <img src="{{asset('assets/images/Berny Julianto.png')}}" alt="berny" />
+                            <div class="contact_text">
+                                <span>Berny Julianto</span>
+                                <li class="d-flex justify-content-center" style="list-style-type: none; margin: 0; padding: 0;">
+                                    as
+                                </li>
+                                <li class="d-flex justify-content-center fw-bold" style="list-style-type: none; margin: 0; padding: 0; color: rgb(255, 50, 77);">
+                                    Writer, Colorist, Editor, CEO
+                                </li>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="contact_wrap contact_style3">
+                            <img src="{{asset('assets/images/Arkan.jpg')}}" alt="arkan" />
+                            <div class="contact_text">
+                                <span>Arkan</span>
+                                <li class="d-flex justify-content-center" style="list-style-type: none; margin: 0; padding: 0;">
+                                    as
+                                </li>
+                                <li class="d-flex justify-content-center fw-bold" style="list-style-type: none; margin: 0; padding: 0; color: rgb(255, 50, 77);">
+                                    Website Developer
+                                </li>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END SECTION EMPLOYEE -->
+                </div>
+            </div>
+        </div>
 @endsection
