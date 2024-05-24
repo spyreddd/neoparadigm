901d42ae6c0a18ca3fbcbbbd428275662fda1e20
spyreddd <adyanizza5353@gmail.com>
2024-05-25 01:27:12 +0700

    pagination update01

--- app/Http/Livewire/Master/CharactersComponent.php
+++ app/Http/Livewire/Master/CharactersComponent.php
@@ -4,11 +4,16 @@ namespace App\Http\Livewire\Master;
 
 use App\Models\Character;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CharactersComponent extends Component
 {
-    public $showing = 4;
+    use WithPagination;
+
     public $perPage = 4;
+
+    protected $paginationTheme = 'bootstrap'; // Use Bootstrap for pagination styling
+
     public function render()
     {
         $characters = Character::latest()->paginate($this->perPage);
--- resources/views/livewire/master/characters-component.blade.php
+++ resources/views/livewire/master/characters-component.blade.php
@@ -22,15 +22,9 @@
         @endforeach
     </div>
     <div class="row">
-        <div class="col-12 mt-2 mt-md-4">
-            <ul class="pagination pagination_style1 justify-content-center">
-                <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1"><i
-                            class="linearicons-arrow-left"></i></a></li>
-                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#"><i class="linearicons-arrow-right"></i></a>
-                </li>
+        <div class="col-12 mt-2 mt-md-4 d-flex justify-content-center">
+            <ul class="pagination pagination_style1 pagination-red">
+                {{ $characters->links() }}
             </ul>
         </div>
     </div>
