354c357019cb89c72ded5423d32ffec07c9a85a7
spyreddd <adyanizza5353@gmail.com>
2024-05-25 14:39:10 +0700

    wallpaper admin

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('content')
     <!-- Page Content -->
-    <div class="bg-image" style="background-image: url('{{ asset('media/photos/photo34@2x.jpg') }}');">
+    <div class="bg-image" style="background-image: url('{{ asset('media/photos/mhw.png') }}');">
         <div class="row mx-0 bg-black-50">
             <div class="hero-static col-md-6 col-xl-8 d-none d-md-flex align-items-md-end">
                 <div class="p-4">
